d7873788d0942a254bc13fa8285e37ee715d256d
Meer Sawood <msawood@redhat.com>
2023-08-30 18:03:26 +0530

    trying to fix: 2

--- prepare_env.sh
+++ prepare_env.sh
@@ -7,10 +7,10 @@ NEW_API_BASE_URL=${API_BASE_URL:-"http://localhost:3005"}
 NEW_WHATSAPPLINK=${WHATSAPPLINK:-"https://new.whatsapp.link"}
 NEW_CHATBOT=${CHATBOT:-"new_config"}
 
-# Replace values in the .env file using sed
-sed -i "s/REACT_APP_API_BASE_URL=.*/REACT_APP_API_BASE_URL=$NEW_API_BASE_URL/" .env
-sed -i "s/REACT_APP_WHATSAPPLINK=.*/REACT_APP_WHATSAPPLINK=$NEW_WHATSAPPLINK/" .env
-sed -i "s/REACT_APP_CHATBOT=.*/REACT_APP_CHATBOT=$NEW_CHATBOT/" .env
+# Replace values in the .env file using sed with a different delimiter
+sed -i "s|REACT_APP_API_BASE_URL=.*|REACT_APP_API_BASE_URL=$NEW_API_BASE_URL|" .env
+sed -i "s|REACT_APP_WHATSAPPLINK=.*|REACT_APP_WHATSAPPLINK=$NEW_WHATSAPPLINK|" .env
+sed -i "s|REACT_APP_CHATBOT=.*|REACT_APP_CHATBOT=$NEW_CHATBOT|" .env
 
 echo "Values replaced in .env file:"
 cat .env
