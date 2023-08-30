f9de9c78497c8f94b174c9687f9802361e5790e1
Meer Sawood <msawood@redhat.com>
2023-08-30 18:28:09 +0530

    final fix

--- prepare_env.sh
+++ prepare_env.sh
@@ -3,9 +3,9 @@ set -ex
 
 cp template.env .env
 # Define the new values you want to set
-NEW_API_BASE_URL=${$API_BASE_URL:-"http://localhost:3005"}
-NEW_WHATSAPPLINK=${$WHATSAPPLINK:-"https://new.whatsapp.link"}
-NEW_CHATBOT=${$CHATBOT:-"new_config"}
+NEW_API_BASE_URL=${API_BASE_URL:-"http://localhost:3005"}
+NEW_WHATSAPPLINK=${WHATSAPPLINK:-"https://new.whatsapp.link"}
+NEW_CHATBOT=${CHATBOT:-"new_config"}
 
 # Replace values in the .env file using sed with a different delimiter
 sed -i "s|REACT_APP_API_BASE_URL=.*|REACT_APP_API_BASE_URL=$NEW_API_BASE_URL|" .env
