ffa2c483aed0153effd84522e1883df3379b90e9
VASHNIROY <vashniroynallapu@gmail.com>
2023-09-29 18:08:14 +0530

    aboutus, polarion,mendix pages modified

--- src/Components/WhatsappIcon/index.js
+++ src/Components/WhatsappIcon/index.js
@@ -1,6 +1,7 @@
 import Lottie from "lottie-react";
 import React from "react";
 import WhatsappTime from "./WhatsappTime.json";
+import { BsWhatsapp } from "react-icons/bs";
 import "./index.css";
 
 // import Whatsappanimation from './whatsappanimation.json'
@@ -9,18 +10,18 @@ const whatsppAPI = process.env.REACT_APP_WHATSAPPLINK;
 
 const WhatsAppIntegration = () => {
   return (
-      <a
-        className="nav-join-whatsapp-anchor mr-5 whatsapp-button"
-        href={whatsppAPI}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Lottie
-          className="whatsapp-icon"
-          loop={true}
-          animationData={WhatsappTime}
-        />
-      </a>
+    <a
+      className="nav-join-whatsapp-anchor mr-5 whatsapp-button"
+      href={whatsppAPI}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Lottie
+        className="whatsapp-icon"
+        loop={true}
+        animationData={WhatsappTime}
+      />
+    </a>
   );
 };
 
